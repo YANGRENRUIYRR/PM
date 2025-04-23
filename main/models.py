2a9182003fe5b2af22046410c86cfa4a98b5058f
杨人瑞 <155858750+YANGRENRUIYRR@users.noreply.github.com>
2025-04-23 18:09:24 +0800

    Upload file: main/models.py

---
+++ main/models.py
@@ -0,0 +1,17 @@
+﻿from django.db import models
+from django.contrib.auth.models import User
+import datetime
+import time
+class Website(models.Model):
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.text
+class Username(models.Model):
+    website = models.ForeignKey(Website, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)#Username
+    text2 = models.CharField(max_length=200)#Password
+    date_added = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return "Username:"+self.text+'\nPassword:'+self.text2
